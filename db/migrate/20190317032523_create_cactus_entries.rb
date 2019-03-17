41cffb0b7c11cfce8db030511e0f34b73ac7aa5d
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-17 03:38:51 +0000

    add attributes to CactusEntries table

--- db/migrate/20190317032523_create_cactus_entries.rb
+++ db/migrate/20190317032523_create_cactus_entries.rb
@@ -1,7 +1,8 @@
 class CreateCactusEntries < ActiveRecord::Migration
   def change
     create_table :cactus_entries do |t|
-
+      t.string :content 
+      t.integer :user_id
       t.timestamps null: false 
     end
   end
