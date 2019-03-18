183bdfe4fa4aedf6def09aa6e9757f6898a4766f
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-18 05:10:30 +0000

    clean up comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,25 @@
 class UsersController < ApplicationController 
   
-  
-  #renders login page 
-  get '/login' do
+  get '/login' do #renders login page 
     erb :login
   end
   
-  #creates a session. Adding a key value paid to the session hash 
-  #receive the login form, find the user, and log the user in (create a session).
   post '/login' do
-    # find the user
       @user = User.find_by(email: params[:email])
-    # authenticate the user 
-      if @user.authenticate(params[:password]) # this code should be in an if statement
-      # test tux, User.find_by(email: "grant@grant.com")
-      # assign variable, grant = _ 
-      # grant, shows user params 
-      # grant.authenticate("goldfish") #=> false 
-      # grant.authenticate("password") #=> returns user info
-    #create session
-        session[:user_id] = @user.id # actually logging the user in. 
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
         puts session
         redirect to "users/#{@user.id}"
       else
-        #tell the user they entered invalid credentials
-        # redirect to the user's landing page
-    # Log the user in 
-    # redirect to the user's landing page 
+        erb :login 
     end
   end
   
-  # render the signup form 
   get '/signup' do
-      
     erb :signup 
   end
   
-  #creating a new user
   post '/users' do
-    # this is where we will create a new user and persist the new user to the db 
     if params[:name] != "" && params[:email] != "" && params[:password] != ""
       @user = User.create(params)
       session[:user_id] = @user.id 
@@ -48,10 +29,8 @@ class UsersController < ApplicationController
     end
   end 
   
-  # user SHOW route
   get '/users/:id' do
     @user = User.find_by(id: params[:id])
-    
     erb :'/users/show'
   end 
   
