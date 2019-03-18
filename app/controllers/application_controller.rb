d1faac31f2900c7af031b20295858e10790e94ec
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-18 02:49:14 +0000

    logged_in and current_user helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,21 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
+  
+  helpers do
+    
+    def logged_in?
+      #true if user is logged in, otherwise false 
+      !!current_user
+    end
+    
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) # eliminates database calls.  
+      # reduces the number of database calls
+      # an instance variable life outside of its own method.  
+      # the first time current_user is referenced within a scope of the instance of ApplicationController this instance variable will be created and assigned if the user is found otherwise it will still be nil. a subsequent call to current_user if already populated won't hit the database again. 
+    end 
+    
+  end 
 
 end
