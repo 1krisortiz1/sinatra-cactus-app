91b38b5a2292e9bace7365e6dccd5a3a71f148ee
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-21 16:19:46 +0000

    complete the post route

--- app/controllers/cactus_entries_controller.rb
+++ app/controllers/cactus_entries_controller.rb
@@ -6,10 +6,22 @@ class CactusEntriesController < ApplicationController
   end 
   
   post '/cactus_entries' do
-    
+    # create a new entry and save it to the database 
+    # only create if the user is logged in
+    if !logged_in?
+      redirect '/'
+    end 
+    # only save the entry if it has some content
+    if params[:content] != ""
+      @cactus_entry = CactusEntry.create(content: params[:content], user_id: current_user.id)
+      redirect "/cactus_entries/#{@cactus_entry.id}"  
+    else
+      redirect '/cactus_entries/new'
+    end
   end
   
   #read 
+  
   
   #update
   
