090288b2bb97ca5408fc0b69270385489a9af58e
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-21 15:53:36 +0000

    add create get and post routes

--- app/controllers/cactus_entries_controller.rb
+++ app/controllers/cactus_entries_controller.rb
@@ -1,3 +1,19 @@
 class CactusEntriesController < ApplicationController
   
+  #create
+  get '/cactus_entries/new' do
+    erb :'/cactus_entries/new'
+  end 
+  
+  post '/cactus_entries' do
+    
+  end
+  
+  #read 
+  
+  #update
+  
+  #delete
+  
+  
 end
