d47eb7163fe4460acf441cb61b1cbcb028bee127
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-31 00:13:59 -0700

    add attributes

--- db/migrate/20190317032523_create_cactus_entries.rb
+++ db/migrate/20190317032523_create_cactus_entries.rb
@@ -1,6 +1,9 @@
 class CreateCactusEntries < ActiveRecord::Migration
   def change
     create_table :cactus_entries do |t|
+      t.string :location
+      t.string :color_flowers
+      t.string :water_conservation_method
       t.string :content 
       t.binary :image
       t.integer :user_id
