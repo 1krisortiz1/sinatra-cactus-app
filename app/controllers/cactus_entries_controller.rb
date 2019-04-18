fc81067273ab5b6bef361ffb176d11e3284283ba
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-04-17 17:23:21 -0700

    remove extra spacing, refactor using redirect_if_not_logged_in

--- app/controllers/cactus_entries_controller.rb
+++ app/controllers/cactus_entries_controller.rb
@@ -2,7 +2,6 @@ class CactusEntriesController < ApplicationController
   
   get '/cactus_entries' do
     @cactus_entries = CactusEntry.all 
-    
     erb :'cactus_entries/index'
   end
   
@@ -13,11 +12,9 @@ class CactusEntriesController < ApplicationController
   
   post '/cactus_entries' do
     redirect_if_not_logged_in 
-
     if params[:content] != "" && params[:image] != "" && params[:location] != "" && params[:color_flowers] != "" && params[:water_conservation_method] != ""
       flash[:message] = "Success!"
       @cactus_entry = CactusEntry.create(location: params[:location], color_flowers: params[:color_flowers], water_conservation_method: params[:water_conservation_method], content: params[:content], image: params[:image], user_id: current_user.id)
-      
       redirect "/cactus_entries/#{@cactus_entry.id}"  
     else
       flash[:error] = "Please complete all fields"
@@ -45,29 +42,25 @@ class CactusEntriesController < ApplicationController
       # find entry, modify(update) entry, redirect?
   patch '/cactus_entries/:id' do 
     set_cactus_entry
-    if logged_in?
+    redirect_if_not_logged_in
       if @cactus_entry.user == current_user && params[:content] != "" && params[:image] != "" && params[:location] != "" && params[:color_flowers] != "" && params[:water_conservation_method] != ""
         @cactus_entry.update(content: params[:content], image: params[:image], location: params[:location], color_flowers: params[:color_flowers], water_conservation_method: params[:water_conservation_method])
         redirect "/cactus_entries/#{@cactus_entry.id}"
       else
         redirect "users/#{current_user.id}"
       end
-    else 
-      redirect '/'
-    end
   end
   
   #delete
   delete '/cactus_entries/:id' do
     set_cactus_entry
-    if logged_in?
+    redirect_if_not_logged_in
       if @cactus_entry.user == current_user
         @cactus_entry.destroy
         redirect '/cactus_entries'
       else
         redirect '/cactus_entries'
       end
-    end
   end
   
   private
